c4b03c2748706f9b9aa363346d9f417cc177e98c
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-12-05 02:52:45 +0100

    day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,30 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(input), do: count_regions(input, &regions_overlap_fully?/1)
+  def part2(input), do: count_regions(input, &regions_overlap?/1)
+
+  def count_regions(input, f) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn r -> r |> String.split(",") |> Enum.map(&parse_region/1) end)
+    |> Enum.filter(f)
+    |> Enum.count()
   end
 
-  def part2(_args) do
+  defp parse_region(region) do
+    [first, last] = region |> String.split("-") |> Enum.map(&String.to_integer/1)
+    first..last
+  end
+
+  defp regions_overlap?([range1, range2]) do
+    !Range.disjoint?(range1, range2)
+  end
+
+  defp regions_overlap_fully?([range1, range2]) do
+    contains(range1, range2) or contains(range2, range1)
+  end
+
+  defp contains(s1..e1, s2..e2) do
+    s1 <= s2 and e1 >= e2
   end
 end
