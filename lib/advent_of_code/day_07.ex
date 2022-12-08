02088500c909f7d7c6205ca945482fadac61d0ec
Ludwig Stecher <ludwig.stecher@gmx.de>
2022-12-08 16:39:56 +0100

    day 7

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,81 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> parse_tree()
+    |> get_dirs(max_size: 100_000)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> parse_tree()
+    |> get_dir_sizes()
+    |> find_deletion_candidate()
+  end
+
+  defp get_dirs(tree, max_size: max_size) do
+    {dir_sizes, _} = get_dir_sizes(tree)
+    Enum.filter(dir_sizes, &(&1 <= max_size))
+  end
+
+  defp find_deletion_candidate({dir_sizes, sum}) do
+    required_space = sum - 40_000_000
+
+    dir_sizes
+    |> Enum.filter(&(&1 >= required_space))
+    |> Enum.min()
+  end
+
+  defp get_dir_sizes(tree) do
+    {dir_sizes, sum} =
+      for {_, value} <- tree, reduce: {[], 0} do
+        {dir_sizes, sum} ->
+          if is_integer(value) do
+            {dir_sizes, sum + value}
+          else
+            {sizes, child_sum} = get_dir_sizes(value)
+            sum = sum + child_sum
+            {sizes ++ dir_sizes, sum}
+          end
+      end
+
+    {[sum | dir_sizes], sum}
+  end
+
+  defp parse_tree(input) do
+    lines = input |> String.split("\n")
+
+    {tree, _} =
+      for line <- lines, reduce: {%{}, []} do
+        {root, path} ->
+          case line do
+            "$ cd /" -> {root, []}
+            "$ cd .." -> {root, tl(path)}
+            "$ cd " <> dir -> {root, [dir | path]}
+            "$ ls" -> {root, path}
+            "dir " <> _ -> {root, path}
+            "" -> {root, path}
+            _ -> {append_rec(root, Enum.reverse(path), parse_file(line)), path}
+          end
+      end
+
+    tree
+  end
+
+  defp parse_file(size_and_name) do
+    [size, name] = String.split(size_and_name, " ")
+    size = String.to_integer(size)
+    {name, size}
+  end
+
+  defp append_rec(root, path, pair) do
+    case path do
+      [head | tail] ->
+        Map.put(root, head, append_rec(Map.get(root, head, %{}), tail, pair))
+
+      [] ->
+        {key, value} = pair
+        Map.put(root, key, value)
+    end
   end
 end
--- test/advent_of_code/day_07_test.exs
+++ test/advent_of_code/day_07_test.exs
@@ -3,19 +3,61 @@ defmodule AdventOfCode.Day07Test do
 
   import AdventOfCode.Day07
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"
+    result = part1(input) == 95437
 
     assert result
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"
     result = part2(input)
 
-    assert result
+    assert result == 24_933_642
   end
 end
